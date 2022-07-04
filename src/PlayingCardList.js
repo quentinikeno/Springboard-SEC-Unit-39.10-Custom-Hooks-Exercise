398b07c6915e9f97481613324c8c8f5514679dc5
Quentin Ikeno <ikenoq@gmail.com>
2022-07-04 12:49:25 -1000

    Add button to remove all cards

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -1,21 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import useAxios from "./hooks/useAxios";
-import uuid from "uuid";
-import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
-	const [cards, addCard] = useAxios(
+	const [cards, addCard, resetCards] = useAxios(
 		"https://deckofcardsapi.com/api/deck/new/draw/"
 	);
 	return (
 		<div className="PlayingCardList">
 			<h3>Pick a card, any card!</h3>
 			<div>
-				<button onClick={addCard}>Add a playing card!</button>
+				<button onClick={() => addCard()}>Add a playing card!</button>
+				<button onClick={resetCards}>Remove all cards!</button>
 			</div>
 			<div className="PlayingCardList-card-area">
 				{cards.map((cardData) => (
