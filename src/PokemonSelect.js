fa4d61acec155f928ddbd4834b3cc359af0c108a
Quentin Ikeno <ikenoq@gmail.com>
2022-07-04 12:51:19 -1000

    Add button to remove all Pokemon

--- src/PokemonSelect.js
+++ src/PokemonSelect.js
@@ -3,25 +3,28 @@ import pokemonList from "./pokemonList";
 import { choice } from "./helpers";
 
 /* Select element to choose from common pokemon. */
-function PokemonSelect({ add, pokemon = pokemonList }) {
-  const [pokeIdx, setPokeIdx] = useState(0);
-  const handleChange = evt => {
-    setPokeIdx(evt.target.value);
-  };
+function PokemonSelect({ add, remove, pokemon = pokemonList }) {
+	const [pokeIdx, setPokeIdx] = useState(0);
+	const handleChange = (evt) => {
+		setPokeIdx(evt.target.value);
+	};
 
-  return (
-    <div>
-      <select onChange={handleChange}>
-        {pokemon.map((p, idx) => (
-          <option key={idx} value={idx}>
-            {p}
-          </option>
-        ))}
-      </select>
-      <button onClick={() => add(pokemon[pokeIdx])}>Catch one!</button>
-      <button onClick={() => add(choice(pokemon))}>I'm feeling lucky</button>
-    </div>
-  );
+	return (
+		<div>
+			<select onChange={handleChange}>
+				{pokemon.map((p, idx) => (
+					<option key={idx} value={idx}>
+						{p}
+					</option>
+				))}
+			</select>
+			<button onClick={() => add(pokemon[pokeIdx])}>Catch one!</button>
+			<button onClick={() => add(choice(pokemon))}>
+				I'm feeling lucky
+			</button>
+			<button onClick={remove}>Remove all Pokemon</button>
+		</div>
+	);
 }
 
 export default PokemonSelect;
