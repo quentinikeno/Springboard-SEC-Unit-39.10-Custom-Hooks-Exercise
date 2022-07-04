5c538d55a562fc57324ed7716a42fc0315008215
Quentin Ikeno <ikenoq@gmail.com>
2022-07-04 13:27:23 -1000

    Allow passing in a parameter for a formatting function.

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -2,12 +2,13 @@ import { useState } from "react";
 import uuid from "uuid";
 import axios from "axios";
 
-const useAxios = (baseUrl, initalState = []) => {
+const useAxios = (baseUrl, dataFormatter, initalState = []) => {
 	const [state, setState] = useState(initalState);
 	const addToState = async (restOfUrl = "") => {
 		const fullUrl = restOfUrl === "" ? baseUrl : `${baseUrl}/${restOfUrl}`;
 		const response = await axios.get(fullUrl);
-		setState((state) => [...state, { ...response.data, id: uuid() }]);
+		const data = dataFormatter(response.data);
+		setState((state) => [...state, { ...data, id: uuid() }]);
 	};
 	const resetState = () => {
 		setState(initalState);
