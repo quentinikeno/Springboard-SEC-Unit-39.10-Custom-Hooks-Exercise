3aced176c0ad905db2987a87e7f9468f0a929aa9
Quentin Ikeno <ikenoq@gmail.com>
2022-07-04 12:49:42 -1000

    Pass function to remove all Pokemon to PokemonSelect

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -1,7 +1,5 @@
 import React from "react";
 import useAxios from "./hooks/useAxios";
-import { v4 as uuid } from "uuid";
-import axios from "axios";
 import PokemonSelect from "./PokemonSelect";
 import PokemonCard from "./PokemonCard";
 import "./PokeDex.css";
@@ -10,12 +8,14 @@ import "./PokeDex.css";
  * Can also add a new card at random,
  * or from a dropdown of available pokemon. */
 function PokeDex() {
-	const [pokemon, addPokemon] = useAxios("https://pokeapi.co/api/v2/pokemon");
+	const [pokemon, addPokemon, removePokemon] = useAxios(
+		"https://pokeapi.co/api/v2/pokemon"
+	);
 	return (
 		<div className="PokeDex">
 			<div className="PokeDex-buttons">
 				<h3>Please select your pokemon:</h3>
-				<PokemonSelect add={addPokemon} />
+				<PokemonSelect add={addPokemon} remove={removePokemon} />
 			</div>
 			<div className="PokeDex-card-area">
 				{pokemon.map((cardData) => (
