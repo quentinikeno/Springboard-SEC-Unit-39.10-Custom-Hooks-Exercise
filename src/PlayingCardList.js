c78c9773d49ea114948b85bb167cb2a540394c84
Quentin Ikeno <ikenoq@gmail.com>
2022-07-04 13:27:53 -1000

    Create formatting function and pass it into useAxios

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -6,8 +6,14 @@ import "./PlayingCardList.css";
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
+	const dataFormatter = (data) => {
+		const { id } = data;
+		const { image } = data.cards[0];
+		return { id, image };
+	};
 	const [cards, addCard, resetCards] = useAxios(
-		"https://deckofcardsapi.com/api/deck/new/draw/"
+		"https://deckofcardsapi.com/api/deck/new/draw/",
+		dataFormatter
 	);
 	return (
 		<div className="PlayingCardList">
@@ -18,10 +24,7 @@ function CardTable() {
 			</div>
 			<div className="PlayingCardList-card-area">
 				{cards.map((cardData) => (
-					<PlayingCard
-						key={cardData.id}
-						front={cardData.cards[0].image}
-					/>
+					<PlayingCard key={cardData.id} front={cardData.image} />
 				))}
 			</div>
 		</div>
